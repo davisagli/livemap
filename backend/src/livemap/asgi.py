fb29cc838548ee25343666251d45e25471e65053
David Glick <david@glicksoftware.com>
2024-11-14 17:44:28 -0800

    Listen on all interfaces

--- backend/src/livemap/asgi.py
+++ backend/src/livemap/asgi.py
@@ -23,7 +23,7 @@ def make_app():
 if __name__ == "__main__":
     uvicorn.run(
         app="livemap.asgi:make_app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8080,
         # reload=True,
     )
